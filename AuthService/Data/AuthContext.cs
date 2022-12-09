7fe84d5af824c893b0b74cb7d78632656d558832
JPychon <64781464+JPychon@users.noreply.github.com>
2022-12-09 12:37:08 -0800

    Fixed a bug with ProviderModel one-to-many entity relation across all DBsets

--- AuthService/Data/AuthContext.cs
+++ AuthService/Data/AuthContext.cs
@@ -8,5 +8,22 @@ namespace AuthService.Data
     {
         public AuthContext(DbContextOptions<AuthContext> options) : base(options) { }
         public virtual DbSet<UserModel> UserCredentials { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProviderModel>()
+            .Property(t => t.Id)
+            .IsRequired()
+            .HasColumnName("Id")
+            .HasColumnType("uniqueidentifier")
+            .HasDefaultValueSql("(newsequentialid())");
+
+            modelBuilder.Entity<List<Guid>>()
+            .HasNoKey();
+
+            modelBuilder.Entity<ProviderModel>()
+           .HasMany(c => c.Authorizations)
+           .WithOne(e => e.ProviderModel);
+        }
     }
 }
