a060822315bde1840bcd068146c170e81a0876c1
Elias Schablowski <eschablowski@me.com>
2022-12-15 19:52:23 -0800

    Add Logging to ClientRepository
    Signed-off-by: Elias Schablowski <eschablowski@me.com>

--- EstimateAPI/Repository/ClientRepository.cs
+++ EstimateAPI/Repository/ClientRepository.cs
@@ -9,20 +9,23 @@ namespace EstimateAPI.Repository
     public class ClientRepository : IClientRepository
     {
         private readonly ClientDiscoveryOptions _options;
+        private readonly ILogger<EstimateController> _logger;
 
         private readonly IKubernetes _kubernetes;
         private readonly string _labelStr;
         private string _namespace;
 
-        public ClientRepository(IOptions<ClientDiscoveryOptions> options) : this(options.Value.Namespace, options) { }
+        public ClientRepository(IOptions<ClientDiscoveryOptions> options, ILogger<ClientRepository> logger) : this(options.Value.Namespace, options, logger) { }
 
-        public ClientRepository(string Namespace, IOptions<ClientDiscoveryOptions> options)
+        public ClientRepository(string Namespace, IOptions<ClientDiscoveryOptions> options, ILogger<ClientRepository> logger)
         {
             _namespace = Namespace;
             _options = options.Value;
+            _logger = logger;
 
             // Get the Kubernetes Object
             var config = KubernetesClientConfiguration.InClusterConfig();
+            _logger.LogDebug($"kubernetes configuration: {config}");
             _kubernetes = new Kubernetes(config);
 
             // Construct the label filter
@@ -33,19 +36,25 @@ namespace EstimateAPI.Repository
             }
 
             _labelStr = string.Join(",", labelStrs.ToArray());
+            _logger.LogDebug($"kubernetes label string: {_labelStr}");
         }
 
         public Estimates.EstimatesClient GetClientByName(string name)
         {
+            _logger.LogDebug($"Requesting client for name: {name}");
             GrpcChannel channel = GrpcChannel.ForAddress($"https://{name}.client");
             return new Estimates.EstimatesClient(channel);
         }
+
         public async Task<List<Estimates.EstimatesClient>> GetClients()
         {
+            _logger.LogDebug($"Getting kubernetes clients");
             var list = await _kubernetes.CoreV1.ListNamespacedServiceAsync(_namespace, labelSelector: _labelStr);
+            _logger.LogDebug($"Received clients: {list}");
             List<Estimates.EstimatesClient> Clients = new List<Estimates.EstimatesClient>();
             foreach (var client in list)
             {
+                _logger.LogDebug($"kubernetes client: {client.Metadata.Name}");
                 Clients.Add(GetClientByName(client.Metadata.Name));
             }
             return Clients;
