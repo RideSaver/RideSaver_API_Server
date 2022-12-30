4400097309fa03d5c029e245f5b148c029f2db92
Elias Schablowski <eschablowski@me.com>
2022-12-30 01:07:51 -0800

    Add logging of access token to Estimates
    Signed-off-by: Elias Schablowski <eschablowski@me.com>

--- EstimateAPI/Controllers/EstimateController.cs
+++ EstimateAPI/Controllers/EstimateController.cs
@@ -30,7 +30,12 @@ namespace EstimateAPI.Controllers
 
             if (AuthenticationHeaderValue.TryParse(authorization, out var headerValue))
             {
+                _logger.LogDebug($"Access Token: {headerValue.Parameter}")
                 token = headerValue.Parameter;
+            }
+            else
+            {
+                _logger.LogDebug($"Access token could not be read: {authorization}")
             }
 
             if (!string.IsNullOrEmpty(token)) { return BadRequest(); }
